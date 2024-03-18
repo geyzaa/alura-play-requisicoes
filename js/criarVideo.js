c4c69be857d9465be8bd2e8493dff586e22ad436
Geyza Oliveira <geyza_amparo@hotmail.com>
2024-03-18 17:21:01 -0300

    Enviar Vídeos

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,12 +1,22 @@
+import { conectaApi } from "./conectaApi.js";
+
 const formulario = document.querySelector("[data-formulario]");
 
-function criarVideo(evento) {
+//as funções que estão conectadas com a Api são assincronas e são declaradas 
+//pelo async e await então elas retorna uma promise, e não teria acesso aqui 
+//até que a promise fosse resolvida, então é necessário transformar essa em assincrona tbm
+async function criarVideo(evento) {
     evento.preventDefault();
     
     const imagem = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem); //parâmetros enviados na mesma ordem que na criaVideo
+
+    window.location.href = "../pages/envio-concluido.html"; 
+   //feedback caso tenha dado certo, redirecionar a pag quando o envio é feito com sucesso
 }
 
 formulario.addEventListener("submit", evento => criarVideo(evento));
